3a537807a335c5acf84d5595c820cc0730670bdc
eveida <eveida.palos@gmail.com>
2023-12-10 17:34:17 -0600

    Primer acercamiento a Java

---
+++ Java Script/script.js
@@ -0,0 +1,49 @@
+// comentario de una sola línea
+/* Comentarios
+Multilinea*/
+
+//alert ("Hola mundo");
+
+//console.log("Este es un menasje para mi consola")
+
+/*var nombre = prompt("Ingresa tu nombre");
+alert("Hola, "+ nombre + " bienvenido(a) al CH35");*/
+
+//Operación en consola
+/*var numero1 = parseInt (prompt("Ingresa el primer numero:"));
+var numero2 = parseInt (prompt("Ingresa el segundo numero:"));
+
+console.log("suma:" + (numero1 + numero2));
+console.log("resta:" + (numero1 - numero2));
+console.log("multiplicacion:" + (numero1 * numero2));
+console.log("division:" + (numero1 / numero2));*/
+
+
+/*var edad = prompt("Ingresa tu edad:");
+
+if (edad >= 18) {
+    alert("Bienvenido a Somos Poker. ¡Disfruta de la página!");
+} else {
+    alert("Lo siento, debes ser mayor de 18 años para acceder a Somos Poker. Acceso denegado.");
+}*/
+
+/*Scope es el alncance de nuestras variables*/
+//scope es el alcance de nuestras variables
+/*var, let, const la diferencia la diferencia es el alcance
+var tiene un alcance global
+let tiene un alcance local
+const se mantiene sin ningún cambio
+*/ 
+
+/*string:cadena de texto (lleva comillas)
+number: numero
+noolena: true/false
+null:nulo
+undefinied:no esta asigando o definido*/
+
+let nombre = "Daniel";
+let invitadxsExtra = 2;
+let esMayorEdad = true;
+let edad = 24;
+let invitadoespecial = null;
+let horaDeSalida = undefined;
